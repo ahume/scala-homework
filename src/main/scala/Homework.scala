b5d32089e388c01034ecb3346e3bb4321b49e4bf
Andy Hume <andyhume@gmail.com>
2012-08-15 19:13:01 +0100

    Got tests passing, at the very least.

--- src/main/scala/Homework.scala
+++ src/main/scala/Homework.scala
@@ -4,7 +4,17 @@ object Homework {
   ////////////////////////////////////
 
   val naughtyWords = List("Celery", "Megatron")
+  lazy val lowerNaughtyWords = naughtyWords map (_.toLowerCase)
+
+  // I wasn't sure what to do with this. When I passed it in to split it did nothing.
   val wordBoundaryRegexPattern = """(?i)(^|\s)%s(\s|$)""" // The %s is for use with the printf style format method
 
-  def isNaughty(text: String) = false
+  def isNaughty(text: String) = {
+
+    val lowerText = text.toLowerCase split (" +")
+
+    // Discovered the exists() method. I feel like I'm cheating.
+    lowerNaughtyWords exists (lowerText contains)
+  }
 }
+
